3e5ebd9928cba9116d3b561f6565200747224f46
Albert Sagol <albert.sagol@gmail.com>
2022-04-14 07:51:24 +0200

    airlines without error but not functional

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
   env: {
     es2021: true,
+    jest: true,
     node: true,
   },
-  extends: ["airbnb-base,prettier,jest,node"],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
   },
-  rules: {},
+  rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+  },
 };
